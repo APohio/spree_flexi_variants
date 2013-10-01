b9842c71c34882b27f203ecbb4f0f4ecda2168c8
ckw2013 <marketing.guru1@gmail.com>
2013-10-01 17:31:57 +1300

    Update add_cart_item_description_to_checkout.rb

--- app/overrides/add_cart_item_description_to_checkout.rb
+++ app/overrides/add_cart_item_description_to_checkout.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => 'spree/checkout/_delivery',
                      :name => 'add_cart_item_description_20130000001',
                      :insert_after => 'code[erb-loud]:contains("item.variant.name")',
                      :text => '<p><%= item %></p><br/>
-                     <p><%= item.products %></p>
+                     <p><%= item.variant.ad_hoc_option_value %></p>
                      <p>This is a test message</p>')
