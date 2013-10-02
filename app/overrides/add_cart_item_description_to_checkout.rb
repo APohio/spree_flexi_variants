80ac960dbf752a37ce6b7a525a87e260d1abce06
ckw2013 <marketing.guru1@gmail.com>
2013-10-02 14:54:52 +1300

    Update add_cart_item_description_to_checkout.rb

--- app/overrides/add_cart_item_description_to_checkout.rb
+++ app/overrides/add_cart_item_description_to_checkout.rb
@@ -3,20 +3,20 @@ Deface::Override.new(:virtual_path => 'spree/checkout/_delivery',
                      :replace => '[data-hook="stock-contents"] tbody',
                      :text => '<tbody>
  <% ship_form.object.manifest.each do |item| %>
- 		<% itemimage = mini_image(item.variant) %>
- 		<% itemname = item.variant.name %>
+ 		<% @itemimage = mini_image(item.variant) %>
+ 		<% @itemname = item.variant.name %>
  		<% order_details = Spree::Order.find_by_id(current_order.id) %>
  		
  		<% order_details.line_items.each do |lineitem| %>
  		
- 		<% itemprice = lineitem.price %>
+ 		<% @itemprice = lineitem.price %>
  		
  			 <% lineitem.ad_hoc_option_values.each do |options| %>
                 <tr class="stock-item">
-                  <td class="item-image"><%= mini_image(itemimage) %></td>
-                  <td class="item-name"><%= itemname %><dt><%= options.option_value.option_type.presentation %></dt><dd><%= options.option_value.presentation %></dd></td>
+                  <td class="item-image"><%= mini_image(@itemimage) %></td>
+                  <td class="item-name"><%= @itemname %><dt><%= options.option_value.option_type.presentation %></dt><dd><%= options.option_value.presentation %></dd></td>
                   <td class="item-qty"><%= options.quantity %></td>
-                  <td class="item-price"><%= itemprice %></td>
+                  <td class="item-price"><%= @itemprice %></td>
                 </tr>
              <% end %>
         <% end %>
