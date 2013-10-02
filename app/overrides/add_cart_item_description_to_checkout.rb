be9217664407c2be81d030663d2ec0e058a0218d
ckw2013 <marketing.guru1@gmail.com>
2013-10-02 14:14:47 +1300

    Update add_cart_item_description_to_checkout.rb

--- app/overrides/add_cart_item_description_to_checkout.rb
+++ app/overrides/add_cart_item_description_to_checkout.rb
@@ -1,8 +1,15 @@
 Deface::Override.new(:virtual_path => 'spree/checkout/_delivery',
                      :name => 'add_cart_item_description_20130000001',
-                     :insert_after => 'code[erb-loud]:contains("display_price(item.variant)")',
-                     :text => '<%= item %>
-                     <% order_details = Spree::Order.find_by_id(current_order.id) %>
+                     :replace => 'code[erb-loud]:contains("display_price(item.variant)")',
+                     :text => '<% order_details = Spree::Order.find_by_id(current_order.id) %>
                      <% order_details.line_items.each do |lineitem| %>
                      <%= lineitem.price %>
+                     <% end %>')
+
+Deface::Override.new(:virtual_path => 'spree/checkout/_delivery',
+                     :name => 'add_cart_item_description_20130000002',
+                     :replace => 'code[erb-loud]:contains("item.variant.name")',
+                     :text => '<% order_details = Spree::Order.find_by_id(current_order.id) %>
+                     <% order_details.line_items.each do |lineitem| %>
+                     <%= lineitem.ad_hoc_option_values %>
                      <% end %>')
