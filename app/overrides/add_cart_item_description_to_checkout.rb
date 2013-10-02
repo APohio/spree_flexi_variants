97922402edf48959dfa9ca2692b0d832cfb8bffc
ckw2013 <marketing.guru1@gmail.com>
2013-10-02 14:45:43 +1300

    Update add_cart_item_description_to_checkout.rb

--- app/overrides/add_cart_item_description_to_checkout.rb
+++ app/overrides/add_cart_item_description_to_checkout.rb
@@ -1,18 +1,22 @@
 Deface::Override.new(:virtual_path => 'spree/checkout/_delivery',
                      :name => 'add_cart_item_description_20130000001',
-                     :replace => 'code[erb-loud]:contains("display_price(item.variant)")',
-                     :text => '<% order_details = Spree::Order.find_by_id(current_order.id) %>
-                     <% order_details.line_items.each do |lineitem| %>
-                     <%= lineitem.price %>
-                     <% end %>')
-
-Deface::Override.new(:virtual_path => 'spree/checkout/_delivery',
-                     :name => 'add_cart_item_description_20130000002',
-                     :insert_after => 'code[erb-loud]:contains("item.variant.name")',
-                     :text => '<% order_details = Spree::Order.find_by_id(current_order.id) %>
-                     <% order_details.line_items.each do |lineitem| %>
-                        <% lineitem.ad_hoc_option_values.each do |options| %>
-                           <dt><%= options.option_value.option_type.presentation %></dt>
-                           <dd><%= options.option_value.presentation %></dd>
-                        <% end %>                    
-                     <% end %>')
+                     :replace => 'code[erb-silent]:contains("ship_form.object.manifest.each")',
+                     :text => '<% ship_form.object.manifest.each do |item| %>
+ 		<% itemimage = mini_image(item.variant) %>
+ 		<% itemname = item.variant.name %>
+ 		<% order_details = Spree::Order.find_by_id(current_order.id) %>
+ 		
+ 		<% order_details.line_items.each do |lineitem| %>
+ 		
+ 		<% itemprice = lineitem.price %>
+ 		
+ 			 <% lineitem.ad_hoc_option_values.each do |options| %>
+                <tr class="stock-item">
+                  <td class="item-image"><%= mini_image(itemimage) %></td>
+                  <td class="item-name"><%= itemname %><dt><%= options.option_value.option_type.presentation %></dt><dd><%= options.option_value.presentation %></dd></td>
+                  <td class="item-qty"><%= options.quantity %></td>
+                  <td class="item-price"><%= itemprice %></td>
+                </tr>
+             <% end %>
+        <% end %>
+ <% end %>')
