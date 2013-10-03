05d04c953bf085420ef287bbc86abd76ad82ffb2
ckw2013 <marketing.guru1@gmail.com>
2013-10-03 16:40:35 +1300

    Update add_customizations_to_cart_form.rb

--- app/overrides/add_customizations_to_cart_form.rb
+++ app/overrides/add_customizations_to_cart_form.rb
@@ -1,4 +1,4 @@
     Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                          :name => "converted_product_price_290656527",
-                         :insert_after => "#product-price",
+                         :insert_before => "#product-price",
                          :partial => "spree/products/customizations")
