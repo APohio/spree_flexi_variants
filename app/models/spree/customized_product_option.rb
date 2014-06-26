ec867c3e1bebc11f7697ddbb2178fb3c506099c3
ckw2013 <marketing.guru1@gmail.com>
2014-06-26 14:35:55 +1200

    Update customized_product_option.rb

--- app/models/spree/customized_product_option.rb
+++ app/models/spree/customized_product_option.rb
@@ -4,8 +4,6 @@ module Spree
   class CustomizedProductOption < ActiveRecord::Base
     belongs_to :product_customization
     belongs_to :customizable_product_option
-    
-    attr_accessible :product_customization_id, :customizable_product_option_id, :value
 
     mount_uploader :customization_image, CustomizationImageUploader
 
