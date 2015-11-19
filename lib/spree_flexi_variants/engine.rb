996f821dc974866ddb997b7ad33931b9e081383b
APohio <adampohio@gmail.com>
2015-11-19 15:08:25 +1300

    Update calculators

--- lib/spree_flexi_variants/engine.rb
+++ lib/spree_flexi_variants/engine.rb
@@ -34,9 +34,7 @@ module SpreeFlexiVariants
       initializer "spree.register.calculators" do |app|
         app.config.spree.calculators.add_class('product_customization_types')
         app.config.spree.calculators.product_customization_types = [
-                                                                    Spree::Calculator::Engraving,
                                                                     Spree::Calculator::AmountTimesConstant,
-                                                                    Spree::Calculator::ProductArea,
                                                                     Spree::Calculator::CustomizationImage
                                                                    ]
       end
